84efeb22d74b0fba1d5f40a1a7404db3f65bc0b3
DannyCoop <cooperdaniel5141@gmail.com>
2020-10-31 06:52:01 -0400

    the extension will grab the grab the urls of the current page. and send it to the backend where it gets a 400 error

--- Extension/app.js
+++
@@ -1,9 +0,0 @@
-const button = document.createElement('button');
-button.textContent = "Grab url"
-document.body.insertAdjacentElement('afterbegin', button);
-
-let url = "";
-let grabUrl = () => [
-    console.log(document.URL)
-]
-button.addEventListener('click', grabUrl) 
