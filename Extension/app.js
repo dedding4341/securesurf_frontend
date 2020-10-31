a1b3e5f48a47575bada39b3b21f951b5cfc882e5
DannyCoop <cooperdaniel5141@gmail.com>
2020-10-30 21:55:45 -0400

    made changes to the manifest files so we can get the url for any urls not just 'http and https', in app.js I wrote a function that grabs the url of what ever site the user is on when a button is clicked. button is for testing

--- Extension/app.js
+++ Extension/app.js
@@ -1,6 +1,7 @@
 const button = document.createElement('button');
-
 button.textContent = "Grab url"
+document.body.insertAdjacentElement('afterbegin', button);
+
 let url = "";
 let grabUrl = () => [
     console.log(document.URL)
