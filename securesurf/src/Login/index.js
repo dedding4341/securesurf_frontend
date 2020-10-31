0064217d963ef1994f18d22f1b7c676c70475bd1
diana <diliang4783573@gmail.com>
2020-10-30 20:15:55 -0700

    added controlled login form, login page component

--- securesurf/src/Login/index.js
+++ securesurf/src/Login/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import LoginForm from "../LoginForm";
+import "./Login.css";
 
 function Login() {
   return (
     <div className="Login">
-      Login page
+      <div className="Login-title">
+        <h1>SecureSurf Login</h1>
+        <p>No account? Install <span className="Login-install-ext" onClick={() => console.log("Redirecting to install extension...")}>SecureSurf</span></p>
+      </div>
+      <LoginForm />
     </div>
   );
 }
